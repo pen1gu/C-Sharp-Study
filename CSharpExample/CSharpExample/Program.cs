3d12d6ef81dc4396efa83b5e6ecb1c67011ce07b
윤현준 <kkkths@naver.com>
2021-02-01 20:24:25 +0900

    create
    test project

---
+++ CSharpExample/CSharpExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Member member1 = new Member();
+            Member member2 = new Member();
+
+            
+        }
+    }
+
+    public class Member
+    {
+        static int count = 50;
+
+        void AddCount(int val)
+        {
+            count += val;
+            Console.WriteLine(count);
+        }
+    }
+
+}
+
